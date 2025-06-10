6b2818eb882d68702eafaa1cb1491760c5460032
nova34tkj4 <novahariyakusuma@gmail.com>
2025-06-10 21:03:27 +0700

    Update pipelineBackend.groovy

--- vars/pipelineBackend.groovy
+++ vars/pipelineBackend.groovy
@@ -8,7 +8,7 @@ def call(String backendStaging) {
         remote.allowAnyHosts = true
 
                 stage('Push Docker image') {
-                    echo "hassssi ${nameservice}"
+                    echo "hassssssi ${nameservice}"
                 }
             }
 }
