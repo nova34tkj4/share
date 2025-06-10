c3857ca6f4b2ddf4aaa26910106f29e657bcba35
nova34tkj4 <novahariyakusuma@gmail.com>
2025-06-10 21:23:47 +0700

    Update pipelineBackend.groovy

--- vars/pipelineBackend.groovy
+++ vars/pipelineBackend.groovy
@@ -2,7 +2,7 @@ def call(String backendStaging) {
     node {
         def remote = [:]
         remote.name = '${nameservice}'
-        remote.host = '34.1.196.45'
+        remote.host = 'xx.xx.xx.xx'
         remote.user = 'ubuntu'
         remote.identityFile = '/var/lib/jenkins/.ssh/id_rsa'
         remote.allowAnyHosts = true
